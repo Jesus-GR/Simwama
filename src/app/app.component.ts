f90277b20a2214744296305694f35bd63c47ba3e
Jesus-GR <jgutiramirez92@gmail.com>
2022-02-03 20:38:14 +0100

    Creacion repositorio

---
+++ src/app/app.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { Product } from './product';
+import { ProductService } from './product.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  products : Observable<Product[]> | undefined;
+
+  //parte del ts para decalrar el formulario
+  productForm = new FormGroup({
+    productId: new FormControl(''),
+    description: new FormControl(''),
+    purchasePrice: new FormControl(0),
+    salePrice: new FormControl(0),
+    stock: new FormControl(0),
+    picture: new FormControl('')
+  }); 
+
+  constructor(private productService:ProductService){
+    this.products = this.productService.getProducts()
+  }
+  formButtonText = 'Add product';
+    
+  addProduct(){
+    this.productService.addProduct(this.productForm.value);
+    //Se le pueden pasar parametros por defecto al resetear.
+    this.productForm.reset({purchasePrice: 0, salePrice: 0, stock: 0});
+  }
+
+  deleteProduct(id: string){
+    this.productService.deleteProduct(id);
+  }
+
+  updateProduct(id : string){
+    this.productService.getProduct(id).subscribe(
+      data => this.productForm.patchValue(data)
+    );
+    this.formButtonText = 'Update product';
+  }
+
+  updateProductStep2(){
+    this.productService.updateProduct(this.productForm.value);
+    this.formButtonText = 'Add product';
+   
+  }
+
+  formSubmit(){
+    (this.formButtonText === 'Add product') ? this.addProduct() : this.updateProductStep2();
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'simwama-aab03',
+    appId: '1:411202699481:web:b149a840662e332c60bec8',
+    storageBucket: 'simwama-aab03.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAj07Vfb4lB6uR71ZG71Lol-LsW4mF4LZs',
+    authDomain: 'simwama-aab03.firebaseapp.com',
+    messagingSenderId: '411202699481',
+  },
+  production: true
+};
